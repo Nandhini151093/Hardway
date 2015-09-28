f4e8200fa852281c9e03234414c752ef6d1665f5
[RUBY] <[nandhinimuthumani@gmail.com]>
2015-09-28 17:27:07 +0530

    Ruby Exercise

---
+++ ex10.rb
@@ -0,0 +1,13 @@
+tabby_cat="\t I am tabbed in."
+persian_cat="I'm split\n on a line"
+backlash_cat="I'm \\a\\ cat"
+fat_cat="""
+I'll do a list:
+\t *cat food
+\t *Fishes
+\t *catnip\n\t*Grass
+"""
+puts tabby_cat
+puts persian_cat
+puts backlash_cat
+puts fat_cat
---
+++ ex11.rb
@@ -0,0 +1,7 @@
+print "How old are you?"
+age=gets.chomp
+print"How tall are you"
+height=gets.chomp
+print"How much do you weight?"
+weight=gets.chomp 
+puts"so,You are #{age} old,#{height} inches tall and #{weight}  lbs heavy."
