a10061caa842c49f25f81bfa00bdb616925ccb37
[RUBY] <[nandhinimuthumani@gmail.com]>
2015-09-28 17:28:01 +0530

    Revert "Ruby Exercise"
    This reverts commit f4e8200fa852281c9e03234414c752ef6d1665f5.

--- ex11.rb
+++
@@ -1,7 +0,0 @@
-print "How old are you?"
-age=gets.chomp
-print"How tall are you"
-height=gets.chomp
-print"How much do you weight?"
-weight=gets.chomp 
-puts"so,You are #{age} old,#{height} inches tall and #{weight}  lbs heavy."
